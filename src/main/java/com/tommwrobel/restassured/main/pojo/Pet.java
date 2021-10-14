21fab6b1dacca394d2591a8dce5989aceb8059ec
Tomasz Wróbel <tomm.wrobel@gmail.com>
2021-10-14 16:52:49 +0200

    Added pojo classes

---
+++ src/main/java/com/tommwrobel/restassured/main/pojo/Pet.java
@@ -0,0 +1,75 @@
+package com.tommwrobel.restassured.main.pojo;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Pet {
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("category")
+    @Expose
+    private Category category;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("photoUrls")
+    @Expose
+    private List<String> photoUrls = null;
+    @SerializedName("tags")
+    @Expose
+    private List<Tag> tags = null;
+    @SerializedName("status")
+    @Expose
+    private String status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getPhotoUrls() {
+        return photoUrls;
+    }
+
+    public void setPhotoUrls(List<String> photoUrls) {
+        this.photoUrls = photoUrls;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
